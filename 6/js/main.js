6d10820a4adaa6b3ba392a088bb3fd7448f63ed0
Keks <keks@htmlacademy.ru>
2022-04-10 22:25:04 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,7 @@
+import './data.js';
+import './util.js';
+import './message.js';
+import './photo.js';
+import './picture.js';
+import './big-picture.js';
+import './gallery.js';
---
+++ 6/js/photo.js
@@ -0,0 +1,22 @@
+import {getUniqueNum, getRandomInt, getRandomArrayElement} from './util.js';
+import {PHOTO_COUNT, DESCRIPTION} from './data.js';
+import {commentsArray} from './message.js';
+
+let uniquePhotoId = [];
+let uniquePictureUrl = [];
+
+const createPhoto = function () {
+  uniquePhotoId = getUniqueNum(1,25,uniquePhotoId);
+  uniquePictureUrl = getUniqueNum(1,25,uniquePictureUrl);
+  return {
+    id: uniquePhotoId.at(-1),
+    url: `photos/${  uniquePictureUrl.at(-1)  }.jpg`,
+    description: getRandomArrayElement(DESCRIPTION),
+    likes: getRandomInt(15, 200),
+    comments: commentsArray,
+  };
+};
+
+const arrayPhoto = Array.from({length: PHOTO_COUNT}, createPhoto);
+
+export {arrayPhoto};
