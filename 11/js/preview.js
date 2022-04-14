445af19631fdd915432efe189e775793ad9ce487
Keks <keks@htmlacademy.ru>
2022-04-14 23:51:41 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/preview.js
+++ 11/js/preview.js
@@ -1,6 +1,7 @@
 import {isEscEvent} from './util.js';
 import {descriptionInput, hashtagsInput, onDescriptionInput, onHashtagInput} from './form.js';
 import {imgPreview, scaleValue, sliderBlockNone, lowerSizeImg, incrementSizeImg} from './effect.js';
+import {CONTROL_MAX_VALUE, EFFECT_CLASS_START, CONTROL_DEFAULT_VALUE} from './const.js';
 
 const fileInput = document.querySelector('#upload-file');
 const imgUploadOverlay = document.querySelector('.img-upload__overlay');
@@ -11,6 +12,9 @@ const hideClass = 'hidden';
 const scaleSmaller = document.querySelector('.scale__control--smaller');
 const scaleBigger = document.querySelector('.scale__control--bigger');
 
+const effectNone = document.querySelector('#effect-none');
+const valueEffectLevel = document.querySelector('.effect-level__value');
+
 const closeUploadFileModal = () => {
   imgUploadOverlay.classList.add(hideClass);
   body.classList.remove('modal-open');
@@ -18,10 +22,13 @@ const closeUploadFileModal = () => {
   hashtagsInput.value = '';
   descriptionInput.value = '';
 
-  scaleValue.value = 100;
+  scaleValue.value = `${CONTROL_MAX_VALUE  }%`;
   imgPreview.style = '';
   imgPreview.style.filter = 'none';
   sliderBlockNone.style.display = 'none';
+  imgPreview.classList.value = `${EFFECT_CLASS_START  }none`;
+  effectNone.checked = true;
+  valueEffectLevel.value = CONTROL_DEFAULT_VALUE;
 
   hashtagsInput.removeEventListener('input', onHashtagInput);
   descriptionInput.removeEventListener('input', onDescriptionInput);
@@ -50,6 +57,8 @@ const onFileInputChange = () => {
   imgUploadOverlay.classList.remove(hideClass);
   body.classList.add('modal-open');
 
+  imgPreview.src = URL.createObjectURL(fileInput.files[0]);
+
   hashtagsInput.addEventListener('input', onHashtagInput);
   descriptionInput.addEventListener('input', onDescriptionInput);
 
@@ -65,11 +74,5 @@ const openUploadFile = () => {
 };
 
 openUploadFile();
-
-// const form = document.querySelector('.img-upload__form');
-// form.addEventListener('submit', (evt) => {
-//   evt.preventDefault();
-//   console.log(form);
-// });
 
 export {onModalCloseButtonClick};
