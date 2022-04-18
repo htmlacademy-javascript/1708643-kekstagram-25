566ba105802aa6aa4d37901a889195f6a5e132f2
margo-chek <38052189+margo-chek@users.noreply.github.com>
2022-04-18 21:42:21 +0300

    перламутровые пуговицы - правки

--- js/gallery.js
+++ js/gallery.js
@@ -1,31 +1,6 @@
 import {showModal} from './big-picture.js';
 import {createPicture, pictureContainer} from './picture.js';
 
-// const clearPictures = () => {
-//   const pictures = pictureContainer.querySelectorAll('.picture');
-//   pictures.forEach((picture) => {
-//     picture.remove();
-//   });
-// };
-
-// const onPictureClick = (evt) => {
-//   evt.preventDefault();
-//   openPictureModal(evt);
-// };
-
-// const onPictureEnterPress = (evt) => {
-//   if (isEnterEvent(evt)) {
-//     evt.preventDefault();
-//     openPictureModal(evt);
-//   }
-// };
-
-// const setPicturesViewed = () => {
-//   const pictures = document.querySelectorAll('a.picture');
-//   pictures.forEach((element) => element.addEventListener('click', onPictureClick));
-//   pictures.forEach((element) => element.addEventListener('keydown', onPictureEnterPress));
-// };
-
 const renderPictures = (photosData) => {
   const fragment = document.createDocumentFragment();
   photosData.forEach((photoData) => {
